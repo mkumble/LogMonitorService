e1c97cee6721406550e23a2b50bc75e1c895ee18
mkumble <mkumble@verizonmedia.com>
2024-02-11 20:45:18 -0800

    feat: Update /api/v1/logs API to accept keyword/text query param, update test cases and README.md

--- src/api/v1/controllers/logsController.js
+++ src/api/v1/controllers/logsController.js
@@ -10,8 +10,9 @@ exports.getLocalLogs = (req, res) => {
     const fileName = req.query.fileName;
     const filePath = path.join(LOG_FILES_BASE_PATH, fileName);
     const numEntries = req.query.numEntries;
+    const keyword = req.query.keyword;
 
-    fileOperations.readFileInReverse(filePath, numEntries, (err, data) => {
+    fileOperations.readFileInReverse(filePath, numEntries, keyword, (err, data) => {
         if (err) {
             return res.status(httpStatus.INTERNAL_SERVER_ERROR).send(LOG_FILE_READ_ERROR);
         }
--- test/api/v1/utils/fileOperations.test.js
+++ test/api/v1/utils/fileOperations.test.js
@@ -6,10 +6,10 @@ const expect = chai.expect;
 const fileOperations = require('../../../../src/api/utils/fileOperations');
 
 describe('readFileInReverse', () => {
-    let readFileStub;
+    let readFileMock;
 
     beforeEach(() => {
-        readFileStub = sinon.stub(fs, 'readFile');
+        readFileMock = sinon.stub(fs, 'readFile');
     });
 
     //restore stubbing after each test case
@@ -17,44 +17,67 @@ describe('readFileInReverse', () => {
         sinon.restore();
     });
 
-    function testReadFileInReverse(mockData, numEntries, done) {
+    function testReadFileInReverse(mockData, numEntries, keyword, done) {
         const expectedData = mockData.split('\n').filter(Boolean).reverse().slice(0, numEntries).join('\n');
-        readFileStub.callsFake((filePath, encoding, callback) => {
+        readFileMock.callsFake((filePath, encoding, callback) => {
             callback(null, mockData);
         });
 
-        fileOperations.readFileInReverse('filePath', numEntries, (err, data) => {
+        fileOperations.readFileInReverse('filePath', numEntries, keyword, (err, data) => {
             expect(err).to.be.null;
             expect(data).to.equal(expectedData);
+            if (keyword) {
+                expect(data.includes(keyword)).to.be.true;
+            }
             done();
         });
     }
 
     //happy path
     it('returns the last n entries when numEntries matches the number of entries in the log file', (done) => {
-        testReadFileInReverse('line1 \n line2 \n line3', 3, done);
+        testReadFileInReverse('line1 \n line2 \n line3', 3, null, done);
     });
 
     it('skips the last line if it is empty', (done) => {
-        testReadFileInReverse('line1 \n line2 \n', 1, done);
+        testReadFileInReverse('line1 \n line2 \n', 1, null, done);
     });
 
     it('returns the last n entries when numEntries is less than the number of entries in the log file', (done) => {
-        testReadFileInReverse('line1 \n line2 \n line3', 2, done);
+        testReadFileInReverse('line1 \n line2 \n line3', 2, null, done);
     });
 
     it('returns all entries when numEntries is greater than the number of entries in the log file', (done) => {
-        testReadFileInReverse('line1 \n line2 \n line3', 4, done);
+        testReadFileInReverse('line1 \n line2 \n line3', 4, null, done);
+    });
+
+    it('returns the last n entries containing the keyword when numEntries matches the number of entries in the log file', (done) => {
+        testReadFileInReverse('line1 \n line2 \n line3', 3, 'line', done);
+    });
+
+    it('returns the last n entries containing the keyword when numEntries matches the number of entries in the log file', (done) => {
+        const mockData = 'line1 \n keyword line2 \n keyword line3';
+        const numEntries = 2;
+        const keyword = 'keyword';
+        const expectedData = mockData.split('\n').filter(line => line.includes(keyword)).reverse().slice(0, numEntries).join('\n');
+        readFileMock.callsFake((filePath, encoding, callback) => {
+            callback(null, mockData);
+        });
+
+        fileOperations.readFileInReverse('filePath', numEntries, keyword, (err, data) => {
+            expect(err).to.be.null;
+            expect(data).to.equal(expectedData);
+            done();
+        });
     });
 
     //error
     it('calls the callback with an error if reading the file fails', (done) => {
         const mockError = new Error('error');
-        readFileStub.callsFake((filePath, encoding, callback) => {
+        readFileMock.callsFake((filePath, encoding, callback) => {
             callback(mockError);
         });
 
-        fileOperations.readFileInReverse('filePath', 2, (err, data) => {
+        fileOperations.readFileInReverse('filePath', 2, null, (err, data) => {
             expect(err).to.equal(mockError);
             expect(data).to.be.undefined;
             done();
