e47080de540daca266ceaa579a75e791e8a7ad8d
mkumble <mkumble@gmail.com>
2024-02-11 22:46:20 -0800

    refactor: Switch from callbacks to async/await for IO operations (#9)
    - switch to async/await for file reads
    - update test cases
    - update README.md

--- src/api/utils/fileOperations.js
+++ src/api/utils/fileOperations.js
@@ -3,39 +3,41 @@ const readline = require('readline');
 const logger = require('./logger');
 const constants = require('./constants')
 
-function readFileInReverse(filePath, numEntries, keyword, callback) {
-    let fileStream;
-    try {
-        fileStream = fs.createReadStream(filePath, { encoding: constants.ENCODING });
-    } catch (err) {
-        logger.log(err, 'error');
-        return callback(err);
-    }
-    const readLineInterface = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity
-    });
-
-    let lines = [];
-    readLineInterface.on('line', (line) => {
-        // if keyword is present, storing only the lines that contain the keyword in memory
-        if (!keyword || line.includes(keyword)) {
-            lines.push(line);
-            //we don't need greater than numEntries lines in memory
-            if (lines.length > numEntries) {
-                lines.shift();  // remove the first line (oldest event)
-            }
+function readFileInReverse(filePath, numEntries, keyword) {
+    return new Promise((resolve, reject) => {
+        let fileStream;
+        try {
+            fileStream = fs.createReadStream(filePath, { encoding: constants.ENCODING });
+        } catch (err) {
+            logger.log(err, 'error');
+            reject(err);
         }
-    });
+        const readLineInterface = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
 
-    readLineInterface.on('close', () => {
-        const reversedLogContent = lines.reverse().join('\n');
-        callback(null, reversedLogContent);
-    });
+        let lines = [];
+        readLineInterface.on('line', (line) => {
+            // if keyword is present, storing only the lines that contain the keyword in memory
+            if (!keyword || line.includes(keyword)) {
+                lines.push(line);
+                //we don't need greater than numEntries lines in memory
+                if (lines.length > numEntries) {
+                    lines.shift();  // remove the first line (oldest event)
+                }
+            }
+        });
 
-    readLineInterface.on('error', (err) => {
-        logger.log(err, 'error');
-        return callback(err);
+        readLineInterface.on('close', () => {
+            const reversedLogContent = lines.reverse().join('\n');
+            resolve(reversedLogContent);
+        });
+
+        readLineInterface.on('error', (err) => {
+            logger.log(err, 'error');
+            reject(err);
+        });
     });
 }
 
