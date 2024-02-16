6485128839198f0ccb63c1cbe18ae57eabd9d98b
mkumble <mkumble@verizonmedia.com>
2024-02-16 12:06:33 -0800

    feat: switch logs read to streams

--- src/api/utils/fileOperations.js
+++ src/api/utils/fileOperations.js
@@ -1,35 +1,12 @@
 const fs = require('fs');
-const readline = require('readline');
+
 const logger = require('./logger');
 const constants = require('./constants')
-const {FileDoesNotExistError, FileStreamReadError} = require('../errors/errorClasses');
-const {FILE_STREAM_READ_ERROR, FILE_STREAM_CREATE_ERROR} = require("../errors/errorMessages");
+const {FileDoesNotExistError, FileStreamCreateError} = require('../errors/errorClasses');
+const {FILE_STREAM_CREATE_ERROR} = require("../errors/errorMessages");
+const LogsTransform = require("../v1/transforms/logsStreamTransform");
 
 //handles reading fileStream data, throws FileStream error if reading fails
-async function handleFileStream(fileStream) {
-    return new Promise((resolve, reject) => {
-        let lines = [];
-        const readLineInterface = readline.createInterface({
-            input: fileStream,
-            crlfDelay: Infinity
-        });
-
-        readLineInterface.on('line', (line) => {
-            lines.push(line);
-        });
-
-        readLineInterface.on('close', () => {
-            resolve(lines);
-        });
-
-        readLineInterface.on('error', (err) => {
-            logger.log(err, FILE_STREAM_READ_ERROR);
-            reject(new FileStreamReadError());
-        });
-    });
-}
-
-//public function for reading file in reverse (uses fileStream)
 function readFileInReverse(filePath, numEntries, keyword) {
     return new Promise((resolve, reject) => {
         if (!fs.existsSync(filePath)) {
@@ -39,31 +16,15 @@ function readFileInReverse(filePath, numEntries, keyword) {
 
         let fileStream;
         try {
-            fileStream = fs.createReadStream(filePath, {encoding: constants.ENCODING});
+            fileStream = fs.createReadStream(filePath, {encoding: constants.UTF8_ENCODING});
         } catch (err) {
             logger.log(err, FILE_STREAM_CREATE_ERROR);
-            reject(new FileStreamReadError());
+            reject(new FileStreamCreateError());
             return;
         }
 
-        handleFileStream(fileStream)
-            .then(lines => {
-
-                // Remove trailing empty lines
-                while (lines.length > 0 && lines[lines.length - 1].trim() === '') {
-                    lines.pop();
-                }
-
-                if (keyword) {
-                    lines = lines.filter(line => line.includes(keyword));
-                }
-                if (numEntries) {
-                    lines = lines.slice(-numEntries);
-                }
-                const jsonLines = lines.reverse().map((line, index) => ({line: index + 1, content: line}));
-                resolve(jsonLines);
-            })
-            .catch(err => reject(err));
+        const logStream = fileStream.pipe(new LogsTransform(numEntries, keyword));
+        resolve(logStream);
     });
 }
 
