c24b6b9fa2b63d974a1812d4c852fd206f2895bb
mkumble <mkumble@verizonmedia.com>
2024-02-16 19:31:56 -0800

    chore: set API response type to application/json, update README

--- src/api/v1/controllers/logsController.js
+++ src/api/v1/controllers/logsController.js
@@ -1,8 +1,10 @@
 const logsService = require('../services/logsService');
 const errorHandlerService = require('../services/errorHandlerService');
+const {CONTENT_TYPE_APPLICATION_JSON} = require('../../utils/constants');
 
 //controller handles marshalling/unmarshalling the request
 exports.getLogsFromServers = async (req, res) => {
+    res.setHeader('Content-Type', CONTENT_TYPE_APPLICATION_JSON);
     try {
         //stream of multiple logs (each log from a server)
         const logsStream = await logsService.getLogs(req.query);
